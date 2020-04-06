14b286cd37cb56e6af4771804d438e0f763cd349
keerthana2002 <55867473+keerthana2002@users.noreply.github.com>
2020-04-06 17:12:34 +0530

    Assigning elements to different lists.
    List1 store the integer value and List2 stores the strings.

---
+++ lists.py
@@ -0,0 +1,13 @@
+list1=[]
+n1=int(input("Enter the number of elements and the integer values: "))
+for i in range(0,n1):
+    elements1=int(input())
+    list1.append(elements1)
+print("List1 = ",list1)  
+list2=[]
+n2=int(input("Enter the number of elements and the strings: "))
+for i in range(0,n2):
+    elements2=input()
+    list2.append(elements2)
+print("List2 = ",list2)    
+    
